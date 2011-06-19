2b42befdbb92b421dac0ef20274e3ee87e464034
Scott Ogle <scott.ogle@escapemg.com>
2011-06-19 01:47:07 -0400

    First commit after installing txp and creating a demo html page.
    Going to bed now.

---
+++ textpattern/config.php
@@ -0,0 +1,9 @@
+<?php
+$txpcfg['db'] = 'txp';
+$txpcfg['user'] = 'root';
+$txpcfg['pass'] = 'root';
+$txpcfg['host'] = 'localhost';
+$txpcfg['table_prefix'] = 'nick';
+$txpcfg['txpath'] = '/Users/scottogle/Sites/Nick/textpattern';
+$txpcfg['dbcharset'] = 'utf8';
+?>
