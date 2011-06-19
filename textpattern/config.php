f48a236982bdf6d130459d2fb46edcd4cfdbae7b
Scott Ogle <scott.ogle@escapemg.com>
2011-06-19 01:56:21 -0400

    added config.php to ignore list

--- textpattern/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$txpcfg['db'] = 'txp';
-$txpcfg['user'] = 'root';
-$txpcfg['pass'] = 'root';
-$txpcfg['host'] = 'localhost';
-$txpcfg['table_prefix'] = 'nick';
-$txpcfg['txpath'] = '/Users/scottogle/Sites/Nick/textpattern';
-$txpcfg['dbcharset'] = 'utf8';
-?>
